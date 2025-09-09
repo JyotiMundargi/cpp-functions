5e6cfc998378e699ae0d319df9e9c5b9adad92cc
Jyoti Mundargi <163743624+JyotiMundargi@users.noreply.github.com>
2025-09-09 22:38:21 +0530

    Add files via upload

---
+++ Square.cpp
@@ -0,0 +1,20 @@
+//Fuctions calling other functions
+#include<iostream>
+using namespace std;
+
+int square(int num)
+{
+    return num*num;
+}
+
+int squareanddoubled(int num)
+{
+    int squared = square(num);
+    return 2 * squared;
+}
+int main()
+{
+    int result = squareanddoubled(10);
+    cout<<"the square of 10 sis " << result <<endl;
+    return 0;
+}
